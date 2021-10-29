2f46630e759a06f9736099b7df3b3ae284212b38
vorontsov-amd <89128818+vorontsov-amd@users.noreply.github.com>
2021-10-29 13:11:49 +0300

    Update 123.c

--- 123.c
+++ 123.c
@@ -12,7 +12,12 @@ struct Rect transform (struct Rect r)
 
 int in_region(struct Point p)
 {
-    if (p.y >= p.x * p.x - 5 && p.y <= 2 && p.x >= -2)
-       return 1;
-    else return 0;
+	if (p.y >= p.x * p.x - 5 && p.y <= 2 && p.x >= -2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
