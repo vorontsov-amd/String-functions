efbe76db731b03639245b7b84e9dc98f8df4a8ca
vorontsov-amd <89128818+vorontsov-amd@users.noreply.github.com>
2021-10-29 13:02:10 +0300

    Update 123.c

--- 123.c
+++ 123.c
@@ -7,3 +7,12 @@ struct Rect transform (struct Rect r)
     temp.rb.y = r.lt.y - (r.rb.x - r.lt.x);
     return temp;
 }
+
+// Задача 2
+
+int in_region(struct Point p)
+{
+    if (p.y >= p.x * p.x - 5 && p.y <= 2 && p.x >= -2)
+       return 1;
+    else return 0;
+}
